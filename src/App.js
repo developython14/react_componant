7e9e199ed7bcb8eff8bb85d4e8e6b4080dc36fd9
developython14 <djmel1955@gmail.com>
2022-12-19 19:47:58 +0100

    push auade about react routing

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import {Free,FREEONE} from './login';
-import { Route, Link, Routes ,Switch ,Redirect  } from 'react-router-dom'
+import { Route, Link, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
-    < Switch >
+    < Routes >
          <Route path="/" component={Free} />
          <Route path="/aboutus" component={ FREEONE } />
-         <Redirect to="/home" />
-   </ Switch >
+   </ Routes >
     </div>
   );
 }
