3ca9e592d8c10017bed3edfddb55e6ece9bf1554
developython14 <djmel1955@gmail.com>
2022-12-20 07:33:20 +0100

    push auade about react routing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     < Routes >
          <Route path="/" element={<Free/>} />
-         <Route path="/aboutus/:username" element={<FREEONE/>} />
+         <Route path="/aboutus" element={<FREEONE/>} />
    </ Routes >
   );
 }
